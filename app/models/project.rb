d5d294d9807351af3308e8a30c027913c2967b56
Jama <jama.bek7@gmail.com>
2014-12-15 12:39:51 -0800

    save file path to db

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,9 +10,25 @@ class Project < ActiveRecord::Base
   validates :slug, length: {minimum: 3}
   validates :slug, uniqueness: true
 
-  scope :low_rate, -> {where("default_rate < 100")}
+  scope :low_rate, -> { where("default_rate < 100") }
 
   def to_s
     "#{name} (#{company})"
+  end
+
+  def self.export_csv(projects)
+    CSV.generate() do |csv|
+      csv << ['name', 'company', 'default_rate', 'created_at', 'owner', 'most recent work item']
+      projects.each do |project|
+        csv << [
+            project.name,
+            project.company,
+            project.default_rate,
+            project.created_at,
+            project.user,
+            project.works.order('created_at DESC').first
+        ]
+      end
+    end
   end
 end
